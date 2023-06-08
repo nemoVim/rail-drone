3b15fc60a027de2349d3120d77ab04e7408176ee
nemoVim <nemovim@kentech.ac.kr>
2023-06-08 18:51:16 +0900

    Add detectBuckling function

--- railway/detection.py
+++ railway/detection.py
@@ -1,10 +1,12 @@
 import cv2 as cv
 import numpy as np
-import time
+
+BUCKLING_TREHSHOLD = 0.35
+LINE_THRESHOLD = 300
 
 def detectLines(original):
     
-    lines = cv.HoughLines(original, 1, np.pi/180, 300)
+    lines = cv.HoughLines(original, 1, np.pi/90, LINE_THRESHOLD)
 
     return lines
 
@@ -53,8 +55,7 @@ def transform(original):
     
     return img
 
-
-def a(img):
+def cut(img):
     img = cv.GaussianBlur(img, (0, 0), 3)
     img = normalize(img)
     img = thresholding(img, np.mean(img)*2)
@@ -66,10 +67,10 @@ def detectRail(original):
     img = original.copy()
 
     img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    hsv = (a(sobel(img)))
+    hsv = (cut(sobel(img)))
     h, s, v = cv.split(hsv)
     img = cv.GaussianBlur(img, (0, 0), 3)
-    h, s, v = a(sobel(h)), a(sobel(s)), a(sobel(v))
+    h, s, v = cut(sobel(h)), cut(sobel(s)), cut(sobel(v))
 
     # cv.imshow("h", (h))
     # cv.imshow("s", (s))
@@ -91,53 +92,61 @@ def detectRail(original):
 
     return img 
 
-def drawLines(original, lines):
+def drawLine(original, r, theta):
 
     img = original.copy()
 
     k = 5000 # 그냥 충분히 큰 값
 
-    if lines is None:
-        return img
+    tx, ty = np.cos(theta), np.sin(theta)
 
-    for i, line in enumerate(lines): # 검출된 모든 선 순회
+    x0, y0 = r*tx, r*ty
 
-        r,theta = line[0] # 거리와 각도 # 가장 중요한 값!
-        # print(f"r: {r}, theta: {theta}")
+    x1, y1 = int(x0 + k*(-ty)), int(y0 + k * tx) # 직선을 그려 줄 시작점과 끝점
+    x2, y2 = int(x0 - k*(-ty)), int(y0 - k * tx)
+    # print(f"(x1, y1): ({x1}, {y1}), (x2, y2): ({x2}, {y2})")
 
-        tx, ty = np.cos(theta), np.sin(theta)
-
-        x0, y0 = r*tx, r*ty
-
-        x1, y1 = int(x0 + k*(-ty)), int(y0 + k * tx) # 직선을 그려 줄 시작점과 끝점
-        x2, y2 = int(x0 - k*(-ty)), int(y0 - k * tx)
-
-        # print(f"(x1, y1): ({x1}, {y1}), (x2, y2): ({x2}, {y2})")
-
-        cv.line(img, (x1, y1), (x2, y2), (0, (i/len(lines))*255, 0), 2) # 초록색 선 그려줌
+    cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2) # 초록색 선 그려줌
 
     return img
 
+def detectBuckling(thetas):
+
+    thetas = np.sin(thetas)
+    maxTheta = np.max(thetas)
+    minTheta = np.min(thetas)
+
+    return (maxTheta - minTheta) > BUCKLING_TREHSHOLD
+
+
+def detect(original):
+
+    detected = detectRail(original) # 이미지를 넣으면 굵은 선들을 감지한 이미지를 반환함
+    lines = detectLines(detected) # 이미지를 넣으면 감지된 선들을 반환함
+
+    # cv.imshow("Detected", detected)
+
+    drawn = original.copy()
+    thetas = []
+    isBuckling = False
+
+    if lines is not None:
+        for line in lines:
+            r, theta = line[0]
+            # print(f"r: {r}, theta: {theta}")
+            drawn = drawLine(drawn, r, theta) # 감지된 선들을 이미지 위에 그려줌
+            thetas.append(theta)
+    
+        isBuckling = detectBuckling(thetas)
+
+        # cv.imshow("Result", drawn)
+    
+    return isBuckling
+
 if __name__ == '__main__':
 
-    # original = cv.resize(cv.imread("./Images/railway_10.jpg"), (1000, 600))
-    original = cv.resize(cv.imread("./Images/railway.jpg"), (1000, 600))
-
-    t_list = np.array([])
-
-    for i in range(100):
-        t = time.time()
-
-        detected = detectRail(original) # 이미지를 넣으면 굵은 선들을 감지한 이미지를 반환함
-        lines = detectLines(detected) # 이미지를 넣으면 감지된 선들을 반환함
-        drawn = drawLines(original, lines) # 감지된 선들을 이미지 위에 그려줌
-
-        t_list = np.append(t_list, [time.time()-t])
-
-
-    print("time: ", np.mean(t_list))
-
-    cv.imshow("Detected", detected)
-    cv.imshow("Result", drawn)
+    original = cv.resize(cv.imread("./Images/railway_10.jpg"), (500, 1000))
+    isBuckling = detect(original)
+    print(f"Buckling: {isBuckling}")
 
     cv.waitKey(0) # 아무 키나 누르면 종료함
--- railway/main.py
+++ railway/main.py
@@ -2,7 +2,7 @@ from detection import *
 from utils import mergeGray2Color
 import cv2 as cv
 
-cap = cv.VideoCapture("./Videos/simulation.mp4")
+cap = cv.VideoCapture("./Videos/morning.mp4")
 
 i = 0
 
@@ -14,8 +14,6 @@ while cap.isOpened():
     suc, frame = cap.read()
 
     if suc:
-        print(i)
-        i += 1
 
         # if i == 100:
         #     frame = cv.resize(frame, (1000, 600))
@@ -26,18 +24,19 @@ while cap.isOpened():
             frame = cv.resize(frame, (1000, 600))
             # if i == 460:
             #     cv.imwrite("./Images/railway_light3.jpg", frame)
-            detected = detectRail(frame) # 이미지를 넣으면 굵은 선들을 감지한 이미지를 반환함
-            lines = detectLines(detected) # 이미지를 넣으면 감지된 선들을 반환함
-            drawn = drawLines(frame, lines) # 감지된 선들을 이미지 위에 그려줌
             # merged = mergeGray2Color(drawn, frame)
             # merged = cv.add(sobel(sobel(frame)), frame)
             # cv.imshow("Hibiscus", drawn)
-            cv.imshow("Hibiscus", drawn)
+            cv.imshow("Hibiscus", frame)
+            if detect(frame):
+                cv.imwrite(f"./Images/buckling_{i}.jpg", frame)
 
             # out.write(merged)
         
         # elif i >= 800:
         #     break
+
+        i += 1
 
         key = cv.waitKey(1)
 
